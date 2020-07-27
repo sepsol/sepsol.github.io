98523a27f29c138418c96aabbbe61debdf017388
SepSol <SR-75@live.com>
2020-07-26 21:42:19 -0700

    added the footer and its icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  Github,
+  Gitlab,
+  Linkedin,
+  Instagram,
+  Twitter,
+} from '@icons-pack/react-simple-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <nav>
+        <ul>
+          <li><a href="#home">HOME</a></li>
+          <li><a href="#about">ABOUT</a></li>
+          <li><a href="#skills">SKILLS</a></li>
+          <li><a href="#work">WORK</a></li>
+        </ul>
+        <ul>
+          <li><a href="#">CONTACT</a></li>
+          <li><a href="#">RESUME</a></li>
+        </ul>
+      </nav>
+      
+      <header id="home">
         <h1>
           Hi, 👋<br/>
           I'm Sepehr,<br/>
@@ -13,6 +34,24 @@ function App() {
         </h1>
         <h3>Get in touch 👉 <a href="mailto:sepsol@techie.com" target="_blank">sepsol@techie.com</a></h3>
       </header>
+      
+      <section id="about"></section>
+      
+      <section id="skills"></section>
+      
+      <section id="work"></section>
+      
+      <footer>
+        <ul>
+          <li><a class="social github" target="_blank" href="https://github.com/sepsol"><Github color="#000" size={24}/></a></li>
+          <li><a class="social gitlab" target="_blank" href="https://gitlab.com/sepsol"><Gitlab color="#000" size={24}/></a></li>
+          <li><a class="social linkedin" target="_blank" href="https://www.linkedin.com/in/sepsol/"><Linkedin color="#000" size={24}/></a></li>
+          <li><a class="social instagram" target="_blank" href="https://www.instagram.com/mr_sepsol/"><Instagram color="#000" size={24}/></a></li>
+          <li><a class="social twitter" target="_blank" href="https://twitter.com/mr_sepsol"><Twitter color="#000" size={24}/></a></li>
+        </ul>
+        <p>Designed and built by Sepehr Soltanieh</p>
+      </footer>
+      
     </div>
   );
 }
