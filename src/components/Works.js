04f2a8c25d04015ad21f5e0a73054787a4bbfdca
SepSol <SR-75@live.com>
2020-08-23 23:06:48 -0700

    added alternating styling for works section

--- src/components/Works.js
+++ src/components/Works.js
@@ -4,21 +4,23 @@ import Data from '../database/works.json';
 
 function Works() {
   return Data.map(work => (
-    <div className="work-card">
-      <div className="work-side">
-        <h4 className="work-title">{work.title}</h4>
-        <p className="work-description">{work.description.short}</p>
-        <ul className="work-techs">
-          {work.techs.map(tech => (
-            <li className="work-tag">{tech}</li>
-          ))}
-        </ul>
+    <div className="work-card-container">
+      <div className="work-card">
+        <div className="work-side">
+          <h4 className="work-title">{work.title}</h4>
+          <p className="work-description">{work.description.short}</p>
+          <ul className="work-techs">
+            {work.techs.map(tech => (
+              <li className="work-tag">{tech}</li>
+            ))}
+          </ul>
+        </div>
+        <img
+          className="work-hero"
+          src={work.img.hero.base64}
+          alt={work.img.hero.alt}
+        />
       </div>
-      <img
-        className="work-hero"
-        src={work.img.hero.base64}
-        alt={work.img.hero.alt}
-      />
     </div>
   ));
 }
