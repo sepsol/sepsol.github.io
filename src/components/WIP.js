ac7d5509295552d4e55df2dd7a5fe9bf8a88db0f
SepSol <SR-75@live.com>
2020-08-28 13:10:47 -0700

    corrected the sizing of wip component

--- src/components/WIP.js
+++ src/components/WIP.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import { useMediaQuery } from 'beautiful-react-hooks';
 
 function WIP() {
+  const desktopScreen = useMediaQuery('(min-width: 720px)');
+  const tabletScreen = useMediaQuery('(min-width: 480px)');
+
   return (
     <section id="wip">
       <img
         src={require('../assets/gif/working-on-it.gif')}
         alt="work in progress gif"
-        style={styles.wip}
+        style={
+          desktopScreen
+            ? styles.desktop
+            : tabletScreen
+            ? styles.tablet
+            : styles.mobile
+        }
       />
     </section>
   );
 }
 
 const styles = {
-  wip: {
+  desktop: {
     display: 'block',
-    margin: '10% auto 0',
+    margin: '0 auto',
+    width: '30%'
+  },
+  tablet: {
+    display: 'block',
+    margin: '0 auto',
     width: '50%'
+  },
+  mobile: {
+    display: 'block',
+    margin: '0 auto',
+    width: '70%'
   }
 };
 
