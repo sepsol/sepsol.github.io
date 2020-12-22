35ae2f0ff4c3d5309ab914866925ba8b2327259e
sepsol <SR-75@live.com>
2020-12-21 17:30:17 -0800

    desktop styling for skills section

--- src/components/Main/Skills.jsx
+++ src/components/Main/Skills.jsx
@@ -4,61 +4,63 @@ import './Skills.scss'
 function Skills() {
   return (
     <div>
-      <div 
-        id="programming-languages"
-        className="skills-subsection"
-      >
-        <h3>Programming Languages</h3>
-        <ul>
-          <li>JavaScript (ES6)</li>
-          <li>TypeScript</li>
-          <li>Dart</li>
-          <li>Java</li>
-          <li>HTML</li>
-          <li>CSS/Sass</li>
-        </ul>
-      </div>
-      <div 
-        id="libraries-and-frameworks"
-        className="skills-subsection"
-      >
-        <h3>Libraries & Frameworks</h3>
-        <ul>
-          <li>React.js</li>
-          <li>React Native</li>
-          <li>Jest</li>
-          <li>Flutter</li>
-          <li>jQuery</li>
-        </ul>
-      </div>
-      <div 
-        id="tools-and-platforms"
-        className="skills-subsection"
-      >
-        <h3>Tools & Platforms</h3>
-        <ul>
-          <li>Git</li>
-          <li>Bash</li>
-          <li>Docker</li>
-          <li>Apollo</li>
-          <li>Postman</li>
-          <li>Chrome DevTools</li>
-          <li>Shopify Polaris</li>
-          <li>Heroku</li>
-        </ul>
-      </div>
-      <div 
-        id="backend-and-database"
-        className="skills-subsection"
-      >
-        <h3>Backend & Database</h3>
-        <ul>
-          <li>Node.js</li>
-          <li>GraphQL</li>
-          <li>Express.js/REST</li>
-          <li>MongoDB/No-SQL</li>
-          <li>MySQL/SQL</li>
-        </ul>
+      <div className="skills-subsection-wrapper">
+        <div 
+          id="programming-languages"
+          className="skills-subsection"
+        >
+          <h4>Programming <br/>Languages</h4>
+          <ul>
+            <li>JavaScript (ES6)</li>
+            <li>TypeScript</li>
+            <li>Dart</li>
+            <li>Java</li>
+            <li>HTML</li>
+            <li>CSS/Sass</li>
+          </ul>
+        </div>
+        <div 
+          id="libraries-and-frameworks"
+          className="skills-subsection"
+        >
+          <h4>Libraries & <br/>Frameworks</h4>
+          <ul>
+            <li>React.js</li>
+            <li>React Native</li>
+            <li>Jest</li>
+            <li>Flutter</li>
+            <li>jQuery</li>
+          </ul>
+        </div>
+        <div 
+          id="tools-and-platforms"
+          className="skills-subsection"
+        >
+          <h4>Tools & <br/>Platforms</h4>
+          <ul>
+            <li>Git</li>
+            <li>Bash</li>
+            <li>Docker</li>
+            <li>Apollo</li>
+            <li>Postman</li>
+            <li>Chrome DevTools</li>
+            <li>Shopify Polaris</li>
+            <li>Heroku</li>
+          </ul>
+        </div>
+        <div 
+          id="backend-and-database"
+          className="skills-subsection"
+        >
+          <h4>Backend & <br/>Database</h4>
+          <ul>
+            <li>Node.js</li>
+            <li>GraphQL</li>
+            <li>Express.js/REST</li>
+            <li>MongoDB/No-SQL</li>
+            <li>MySQL/SQL</li>
+          </ul>
+        </div>
       </div>
       <footer>
         Other technologies I've worked with include but are not limited to 
